81104063096127ddd02a535f0b9eec1edd143d5f
DaniellePashayan <daniellepashayan@gmail.com>
2024-07-13 10:52:28 -0400

    added .bat for server run

---
+++ run.bat
@@ -0,0 +1,9 @@
+@echo off
+REM Navigate to the directory containing your Python script
+cd C:\Users\Administrator\Documents\Python Scripts\ezyvet_datalake
+
+REM Activate your virtual environment if you are using one
+call .venv\Scripts\activate
+
+REM Run the Python script
+python main.py
